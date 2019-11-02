6b8a9d4bcc24c0b69c82fee79550e5235dc76aea
Edvin Dunaway <git@eddinn.net>
2019-11-02 18:13:07 +0000

    added sanity check of user input, update the README.md

--- README.md
+++ README.md
@@ -13,14 +13,28 @@ sudo ./ddusb.sh /path/to/some.iso
 ### Sample output
 
 ```shell
-Identify the USB disk device
+Identify the USB device:
 sdf                   8:0    1   7,5G  0 disk
 ├─sdf1                8:1    1   2,3G  0 part /media/username/Ubuntu 19.10 amd64
 ├─sdf2                8:2    1   3,9M  0 part
 └─sdf3                8:3    1   5,2G  0 part /media/username/casper-rw
 
-Please enter the USB disk identifier noted above (e.g sdf)
-Identifier: sdf
+Please enter the USB device identifier noted above with full path (e.g /dev/sdf)
+Identifier: /dev/sdf
 
-Creating bootable USB of ISO /home/username/Downloads/pop-os_19.10_amd64_nvidia_9.iso to USB device /dev/sdf
+Please verify that all input is correct:
+
+ISO file: /path/to/some.iso
+USB device: /dev/sdf
+dd will be: dd bs=4M if=/path/to/some.iso of=/dev/sdf status=progress oflag=sync
+
+Is the input correct? [y/n]: y
+
+Creating bootable USB of ISO /path/to/some.iso to USB device /dev/sdf
+2535194624 bytes (2,5 GB, 2,4 GiB) copied, 415 s, 6,1 MB/s
+604+1 records in
+604+1 records out
+2535194624 bytes (2,5 GB, 2,4 GiB) copied, 415,021 s, 6,1 MB/s
+
+Done!
 ```
--- ddusb.sh
+++ ddusb.sh
@@ -2,8 +2,8 @@
 
 # Check arguments, exit if none
 if [ $# -eq 0 ]; then
-  printf -- '%s\n' "Missing full path to the ISO" >&2
-  printf -- '%s\n' "Usage: sudo ./ddusb.sh /path/to/some.iso" >&2
+  printf -- '%s\n\n' "Missing full path to the ISO" >&2
+  printf -- '%s\n\n' "Usage: sudo ./ddusb.sh /path/to/some.iso" >&2
   exit 1
 fi
 
@@ -14,21 +14,46 @@ if (( EUID != 0 )); then
 fi
 
 # Run lsblk and find the USB identifier 
-printf -- '%s\n' "Identify the USB device"
+printf -- '%s\n' "Identify the USB device:"
 lsblk | grep "sd"
 
 # Get the identifier from the user
 printf -- '%s\n' ""
-printf -- '%s\n' "Please enter the USB dev identifier noted above (e.g sda)"
+printf -- '%s\n' "Please enter the USB device identifier noted above with full path (e.g /dev/sdf)"
 printf "Identifier: "; read -r usb
 
 # Check if we got any input and continue, exit if none
 if [ -z "$usb" ]; then
-  printf -- '%s\n' ""
   printf -- '%s\n' "Missing USB identifier, exiting!"
   exit 1
 else
-  printf -- '%s\n' ""
-  printf -- '%s\n' "Creating bootable USB of ISO $1 to USB device /dev/$usb"
-  (dd bs=4M if="$1" of=/dev/"$usb" status=progress oflag=sync)
+  if ! file "$usb" | grep -c "block special" > /dev/null 2>&1; then
+    printf -- '%s\n' "$usb is not a valid target, aborting!"
+    exit 1
+  else
+    printf -- '%s\n' ""
+    printf -- '%s\n\n' "Please verify that all input is correct:"
+    printf -- '%s\n' "ISO file: $1"
+    printf -- '%s\n' "USB device: $usb"
+    printf -- '%s\n\n' "dd will be: dd bs=4M if=$1 of=$usb status=progress oflag=sync"
+    read -rp $'Is the input correct? [y/n]: ' -n1 key
+    case "${key}" in
+      (y)
+        printf -- '%s\n' "Creating bootable USB of ISO $1 to USB device $usb"
+        (dd bs=4M if="$1" of="$usb" status=progress oflag=sync)
+        printf -- '%s\n' ""
+        printf -- '%s\n' "Done!"
+        ;;
+      (n)
+        printf -- '%s\n' ""
+        printf -- '%s\n' "Aborting!" >&2
+        exit 1
+        ;;
+      (*)
+        printf -- '%s\n' ""
+        printf -- '%s\n' "Invalid input, aborting!" >&2
+        exit 1
+        ;;
+    esac
+  fi
 fi
